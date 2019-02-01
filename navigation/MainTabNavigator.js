b0ea0be809ad6d41483e67d84dadde5f51b337eb
Ahmed Alsaab Home <alsaaba@cardiff.ac.uk>
2019-02-01 17:35:28 +0000

    First commit. ReactNative initialized with Expo. Incorperated Nodejs, expressjs, mysql and nodemon as modules. Autogenerated react-native-navigation during project creation. Renamed and adjusted base screens: These are now named Journey, Tickets, Account. Added a 4th screen/file called Wallet and embedd it onto the navigation. This screen can now be navigated to. Icons also changed to correspond to the new screen names

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import JourneyScreen from '../screens/JourneyScreen';
+import TicktetsScreen from '../screens/TicktetsScreen';
+import AccountsScreen from '../screens/AccountsScreen';
+import WalletScreen from '../screens/WalletScreen';
+
+const JourneyStack = createStackNavigator({
+  Home: JourneyScreen,
+});
+
+JourneyStack.navigationOptions = {
+  tabBarLabel: 'Journey',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-bus' : 'md-bus'}
+    />
+  ),
+};
+
+const TicketsStack = createStackNavigator({
+  Ticket: TicktetsScreen,
+});
+
+TicketsStack.navigationOptions = {
+  tabBarLabel: 'Tickets',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-pricetags' : 'md-pricetags'}
+    />
+  ),
+};
+
+const WalletStack = createStackNavigator({
+  Links: WalletScreen,
+});
+
+WalletStack.navigationOptions = {
+  tabBarLabel: 'Wallet',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-card' : 'md-card'}
+    />
+  ),
+};
+
+const AccountStack = createStackNavigator({
+  Account: AccountsScreen,
+});
+
+AccountStack.navigationOptions = {
+  tabBarLabel: 'Account',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  JourneyStack,
+  TicketsStack,
+  WalletStack,
+  AccountStack,
+  
+});
