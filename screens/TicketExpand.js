6e34b260c6a9c17575a11a3459142acd387cc158
Joanna Araneta <joannaAraneta@Idunahs-MacBook-Air.local>
2019-02-19 00:40:53 +0000

    fixed the error on the expansion not opening and closing

--- screens/TicketExpand.js
+++ screens/TicketExpand.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     Animated,
+    Easing,
+    Image,
     Dimensions,
     StyleSheet,
     Text,
@@ -8,72 +10,91 @@ import {
     View
 } from 'react-native';
 
-const {width, height} = Dimensions.get('window');
- 
-export default class TicketExpand extends Component{
+const { width, height } = Dimensions.get('window');
 
-    state= {
-        position: new Animated.Value(this.props.isOpen ? 0 :height),
+export default class TicketExpand extends Component {
+
+    state = {
+        position: new Animated.Value(0),
         visible: this.props.isOpen,
     };
 
-    componentWillReceiveProps(nextProps){
-        if(!this.props.isOpen && nextProps.isOpen){
-            this.animateClose();
+    // componentDidMount(){
+    //     this.setState({
+    //         position: new Animated.Value(this.props.isOpen ? height : 0)
+    //     })
+    //     console.log(this.state.position)
+
+    // }
+
+    componentWillUpdate(nextProps){
+        if(this.props.isOpen === true && this.props.isOpen !== nextProps.isOpen){
+            this.animateOpen();
         }
     }
 
-    animateOpen(){
-        this.setState({visible:true}, () => {
-            Animated.timing(
-                this.setState.position, {toValue: 0}
-            ).start();
-        });
-        }
-        
-        animateClose(){
-            Animated.timing(
-                this.setState.position, {toValue: height}
-            ).start(() => this.setState({visible:false}));
+    componentWillReceiveProps(nextProps) {
+        if (!this.props.isOpen && nextProps.isOpen) {
+            this.animateOpen();
+        } 
+    }
+
+    animateOpen = () => {
+        this.setState({ visible: true, position: height })
+        // Animated.timing(
+        //     this.state.position,
+        //     { 
+        //         toValue: height,
+        //         duration: 1000,
+        //         easing: Easing.linear
+        // },
+        // ).start();
+    }
+
+    animateClose = () => {
+        this.setState({ 
+            visible: false,
+            position: 0 
+        })
+    }
+
+    render() {
+        if (!this.state.visible) {
+            return null;
         }
 
-        render(){
-            if(!this.state.visible){
-                return null;
-            }
-
-            return(
-                <View style = {StyleSheet.container}>
-                <TouchableWithoutFeedback onPress={this.props.onClose}>
-                <Animated.View style={StyleSheet.backdrop}/>
+        return (
+            <View style={styles.container}>
+                <TouchableWithoutFeedback onPress={this.animateClose}>
+                    <Animated.View style={styles.backdrop} />
                 </TouchableWithoutFeedback>
                 <Animated.View
-                style={[styles.modal, {
-                    transform:[{translateY:this.state.position}, {translateX:0}]
-                }]}
+                    style={[styles.modal, {
+                        transform: [{ translateY: this.state.position }, { translateX: 0 }]
+                    }]}
                 >
-                <Text>Expansion of Ticket</Text>
+                    <Text>Expansion of Ticket</Text>
                 </Animated.View>
 
-                </View>
-            );
-        }
-    
+            </View>
+        );
     }
 
-    const styles = StyleSheet.create({
-        container:{
-            ...StyleSheet.absoluteFillObject,
-            justifyContent: 'flex-end',
-            backgroundColor: 'transparent',
-        },
-        backdrop:{
-            ...StyleSheet.absoluteFillObject,
-            backgroundColor:'black',
-            opacity: 0.5,
-        },
-        modal:{
-            height: height/2,
-            backgroundColor:'white',
-        },
-    });
+}
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        backgroundColor: 'transparent',
+    },
+    backdrop: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'black',
+        opacity: 0.5,
+    },
+    modal: {
+        height: height / 2,
+        backgroundColor: 'white',
+    },
+});
