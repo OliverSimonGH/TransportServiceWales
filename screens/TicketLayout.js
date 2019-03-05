af315e9029774a40715883b33f1f8a6066bb2b0c
Joanna Araneta <joannaAraneta@Idunahs-MacBook-Air.local>
2019-03-05 13:11:17 +0000

    styled the tickets

--- screens/TicketLayout.js
+++ screens/TicketLayout.js
@@ -18,67 +18,78 @@ const cols = 3, rows = 3;
 export default class TicketLayout extends Component {
 
     static navigationOptions = {
-		header: null
-	};
+        header: null
+    };
 
-	state = {
-		expansionIsOpen: false,
-		isLoadingComplete: false,
-		ticketData: []
-	};
+    state = {
+        expansionIsOpen: false,
+        isLoadingComplete: false,
+        ticketData: []
+    };
 
-	componentDidMount() {
+    componentDidMount() {
         const id = this.props.ticketId;
 
-		fetch(`http://${ip}:3000/ticketsQuery?id=${id}`).then((response) => response.json()).then((response) => {
-			console.log(response.ticket);
-			this.setState({
-				ticketData: response.ticket
-			});
-		});
-	}
+        fetch(`http://${ip}:3000/ticketsQuery?id=${id}`).then((response) => response.json()).then((response) => {
+            console.log(response.ticket);
+            this.setState({
+                ticketData: response.ticket
+            });
+        });
+    }
 
     render() {
         return (
-            <View style={{backgroundColor: 'transparent'}}> 
+            <View style={{ backgroundColor: 'transparent', paddingBottom: 10}}>
                 <Content>
-                <ImageBackground source={require('../assets/images/active-tickets.png')} style={{ width: '100%', height: '100%' }}>
+                    <ImageBackground source={require('../assets/images/active-tickets.png')} style={{ width: 300, height: 250, justifyContent: 'center' }}>
 
-                
-                                <Left>
-                                    <Body>
-                                        <TouchableOpacity style={styles.container} onPress={this.props.onOpen}>
-                                        
-                                        {this.state.ticketData.length >= 1 && (
-								<View style={styles.container}>
-									<React.Fragment>
-										<Text>
-											City From:
+
+                        <Left>
+                            <Body>
+                                <TouchableOpacity style={styles.container} onPress={this.props.onOpen}>
+
+                                    {this.state.ticketData.length >= 1 && (
+                                        <View style={styles.container}>
+                                            <React.Fragment>
+                                                <Text>
+                                                    City From:
 											{this.state.ticketData[0].city}
-										</Text>
-										<Text>
-											{' '}
-											Departure: 
+                                                </Text>
+                                                <Text>
+                                                    {' '}
+                                                    Departure:
 											{moment(this.state.ticketData[0].start_time).format("dddd Do h:mm a")}
-										</Text>
-								
-										<Text>
-											To City:
+                                                </Text>
+                                                <View style={styles.imageContainer}>
+                                                    <Image
+                                                        source={require('../assets/images/qrcode.jpg')}
+                                                        style={{
+                                                            width: 150,
+                                                            height: 150,
+                                                            borderRadius: 10,
+                                                            alignSelf: 'center',
+                                                            backgroundColor: 'transparent',
+                                                        }}
+                                                    />
+                                                </View>
+                                                <Text>
+                                                    To City:
 											{this.state.ticketData[1].city}
-										</Text>
-										<Text>
-											{' '}
-											Arrival: 
+                                                </Text>
+                                                <Text>
+                                                    {' '}
+                                                    Arrival:
 											{moment(this.state.ticketData[1].end_time).format("dddd Do h:mm a")}
-										</Text>
-									</React.Fragment>
-								</View>
-							)}
-                                        </TouchableOpacity>
-                                    </Body>
+                                                </Text>
+                                            </React.Fragment>
+                                        </View>
+                                    )}
+                                </TouchableOpacity>
+                            </Body>
 
-                                </Left>
-                       
+                        </Left>
+
                     </ImageBackground>
 
                 </Content>
