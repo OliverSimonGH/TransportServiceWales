9af234317bb1e7da37101110d5b08b9704cca412
OliverSimonGH <oliver_simon@hotmail.co.uk>
2019-03-27 18:17:00 +0000

    Refactored the result screen filtering from result journey to result screen due to journeys showing that have been filtered but disappear after a second

--- screens/passenger/ResultJourney.js
+++ screens/passenger/ResultJourney.js
@@ -1,131 +1,21 @@
 import React, { Component } from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
-import ip from '../../ipstore'
-import key from '../../google_api_key'
 import moment from 'moment';
-import { getRequestAuthorized } from '../../API' 
-
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class ResultJourney extends Component {
 
-  state = {
-    startDate: null,
-    endDate: null,
-    newTotalTime: 0,
-    departDays: '',
-    departHours: '',
-    departMinutes: '',
-    totalHours: 0,
-    totalMinutes: 0
-  }
-
-  componentDidMount() {
-    const { id, lat, lng } = this.props;
-
-    getRequestAuthorized(`http://${ip}:3000/journey?journeyId=${id}`)
-      .then(response => {
-        var start = '', end = '', waypoints = [{ latitude: lat, longitude: lng }]
-
-        this.setState({
-          startDate: response.start_time,
-          endDate: response.end_time
-        })
-
-        for (let i = 0; i < response.results.length; i++) {
-          const element = response.results[i];
-          // console.log(response)
-          switch (element.fk_coordinate_type_id) {
-            //Start
-            case 1:
-              start = element.place_id;
-              break;
-
-            //End
-            case 2:
-              end = element.place_id;
-              break;
-
-            //Virtual bus Stop
-            case 3:
-              waypoints.push({ latitude: element.latitude, longitude: element.longitude })
-              break
-          }
-
-        }
-
-        fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${key}&origin=place_id:${start}&destination=place_id:${end}&waypoints=${this.waypointArrayToString(waypoints)}`)
-          .then(response => response.json())
-          .then(response => {
-            response.routes[0].legs.map(leg => {
-              this.setState({
-                newTotalTime: parseInt(this.state.newTotalTime) + parseInt(leg.duration.value)
-              })
-            })
-          })
-          .then(() => {
-            var startDate = moment(this.state.startDate).toISOString();
-            var endDate = moment(this.state.endDate).toISOString();
-            var nowDate = moment(new Date()).toISOString();
-
-            var duration = moment.duration(moment(endDate).diff(startDate))
-            var departDuration = moment.duration(moment(endDate).diff(nowDate))
-            console.log(departDuration.asSeconds())
-
-            var totalHours = parseInt(duration.asHours());
-            var totalMinutes = parseInt(duration.asMinutes()) % 60;
-            var totalTotalMinutes = parseInt(duration.asMinutes());
-
-            var departDays = parseInt(departDuration.asDays());
-            var departHours = parseInt(departDuration.asHours());
-            var departMinutes = parseInt(departDuration.asMinutes()) % 60;
-
-            var newTotalTime = parseInt(this.state.newTotalTime / 60);
-
-            if (newTotalTime >= totalTotalMinutes || departDuration.asSeconds() <= 0) {
-              this.props.remove()
-            }
-            else {
-              this.setState({
-                departDays: departDays,
-                departHours: departHours,
-                departMinutes: departMinutes,
-                totalHours: totalHours,
-                totalMinutes: totalMinutes
-              })
-            }
-          })
-      })
-  }
-
-  waypointArrayToString = (waypointArray) => {
-    var waypoints = ''
-
-    for (let i = 0; i < waypointArray.length; i++) {
-      const element = waypointArray[i];
-
-      if (i >= 1) {
-        waypoints += `%7C${element.latitude},${element.longitude}`;
-        continue;
-      }
-
-      waypoints += `${element.latitude},${element.longitude}`
-    }
-
-    return waypoints
-  }
-
   render() {
     return (
       <TouchableOpacity style={{ padding: 10, borderBottomColor: '#dfdfdf', borderBottomWidth: 1, flexDirection: "row", alignItems: 'center' }} onPress={this.props.onClick}>
-        <View style={{ flex: 1 }}><Text style={{ fontWeight: 'bold' }}>{`${this.state.departDays} days \n ${this.state.departHours} hours \n ${this.state.departMinutes} mins`}</Text></View>
+        <View style={{ flex: 1 }}><Text style={{ fontWeight: 'bold' }}>{`${this.props.departDays} days \n ${this.props.departHours} hours \n ${this.props.departMinutes} mins`}</Text></View>
         <View style={{ flexDirection: 'row', flex: 5 }}>
           <View style={{ flexDirection: 'column', flex: 1, marginLeft: 20, marginRight: 20 }}>
             <View style={{ flexDirection: "row", justifyContent: 'flex-start' }}><Icon name="md-walk" size={25} style={{ marginRight: 20 }} /><Icon name="md-bus" size={25} /></View>
 
             <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
-              <Text>{moment(this.state.startDate).format('hh:mm a')} - {moment(this.state.endDate).format('hh:mm a')}</Text>
-              <Text>{`${this.state.totalHours} hr ${this.state.totalMinutes} min`}</Text>
+              <Text>{moment(this.props.startDate).format('hh:mm a')} - {moment(this.props.endDate).format('hh:mm a')}</Text>
+              <Text>{`${this.props.totalHours} hr ${this.props.totalMinutes} min`}</Text>
             </View>
           </View>
         </View>
--- screens/passenger/ResultScreen.js
+++ screens/passenger/ResultScreen.js
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native'
 import { Container, Content } from 'native-base';
 import Icon from 'react-native-vector-icons/Entypo';
 import ip from '../../ipstore'
-import { getRequestAuthorized } from '../../API' 
+import key from '../../google_api_key'
+import moment from 'moment';
+import { getRequestAuthorized } from '../../API'
 
 import GlobalHeader from '../../components/GlobalHeader';
 import ResultJourney from './ResultJourney'
@@ -13,7 +15,7 @@ class ResultScreen extends Component {
     static navigationOptions = {
         header: null
     };
-   
+
     state = {
         journey: []
     }
@@ -25,23 +27,101 @@ class ResultScreen extends Component {
     }
 
     onJourneyPress = (id) => {
-        this.props.navigation.navigate('Summary', { jData: this.props.navigation.state.params, jId: id});
+        this.props.navigation.navigate('Summary', { jData: this.props.navigation.state.params, jId: id });
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const { lat, lng, endCity, endStreet, numPassenger } = this.props.navigation.state.params;
 
         getRequestAuthorized(`http://${ip}:3000/journeyResults?street=${endStreet}&city=${endCity}`)
-        .then(response => {
-            if  (response.results.length < 1) return;
-            for (let i = 0; i < response.results.length; i++) {
-                const id = response.results[i].journey_id;
-                this.setState({
-                    journey: [...this.state.journey, <ResultJourney key={id} id={id} lat={lat} lng={lng} remove={() => this.removeOverTimeLimitJourneys(id)} onClick={() => this.onJourneyPress(id)} passengers={numPassenger}/>]
-                })
+            .then(response => {
+                if (response.results.length < 1) return;
+                for (let i = 0; i < response.results.length; i++) {
+                    const id = response.results[i].journey_id;
+
+                    //Filter result list
+                    getRequestAuthorized(`http://${ip}:3000/journey?journeyId=${id}`)
+                        .then(response => {
+                            var start = '', end = '', waypoints = [{ latitude: lat, longitude: lng }]
+                            var startDate1 = response.start_time, endDate1 = response.end_time;
+
+                            for (let i = 0; i < response.results.length; i++) {
+                                const element = response.results[i];
+                                // console.log(response)
+                                switch (element.fk_coordinate_type_id) {
+                                    //Start
+                                    case 1:
+                                        start = element.place_id;
+                                        break;
+
+                                    //End
+                                    case 2:
+                                        end = element.place_id;
+                                        break;
+
+                                    //Virtual bus Stop
+                                    case 3:
+                                        waypoints.push({ latitude: element.latitude, longitude: element.longitude })
+                                        break
+                                }
+
+                            }
+
+                            fetch(`https://maps.googleapis.com/maps/api/directions/json?key=${key}&origin=place_id:${start}&destination=place_id:${end}&waypoints=${this.waypointArrayToString(waypoints)}`)
+                                .then(response => response.json())
+                                .then(response => {
+                                    let newTotalTime = 0
+                                    response.routes[0].legs.map(leg => newTotalTime += parseInt(leg.duration.value))
+                                    return Promise.resolve(newTotalTime)
+                                })
+                                .then((newTotalTime) => {
+                                    var startDate = moment(startDate1).toISOString();
+                                    var endDate = moment(endDate1).toISOString();
+                                    var nowDate = moment(new Date()).toISOString();
+
+                                    var duration = moment.duration(moment(endDate).diff(startDate))
+                                    var departDuration = moment.duration(moment(endDate).diff(nowDate))
+
+                                    var totalHours = parseInt(duration.asHours());
+                                    var totalMinutes = parseInt(duration.asMinutes()) % 60;
+                                    var totalTotalMinutes = parseInt(duration.asMinutes());
+
+                                    var departDays = parseInt(departDuration.asDays());
+                                    var departHours = parseInt(departDuration.asHours());
+                                    var departMinutes = parseInt(departDuration.asMinutes()) % 60;
+
+                                    var newTotalTime = parseInt(newTotalTime / 60);
+
+                                    if (newTotalTime >= totalTotalMinutes || departDuration.asSeconds() <= 0) {
+                                        return;
+                                    }
+                                    else {
+                                        this.setState({
+                                            journey: [...this.state.journey, <ResultJourney key={id} departDays={departDays} departHours={departHours} departMinutes={departMinutes} totalHours={totalHours} totalMinutes={totalMinutes} startDate={startDate} endDate={endDate} onClick={() => this.onJourneyPress(id)} passengers={numPassenger} />]
+                                        })
+                                    }
+                                })
+                        })
+                }
+            })
+    }
+
+    waypointArrayToString = (waypointArray) => {
+        var waypoints = ''
+
+        for (let i = 0; i < waypointArray.length; i++) {
+            const element = waypointArray[i];
+
+            if (i >= 1) {
+                waypoints += `%7C${element.latitude},${element.longitude}`;
+                continue;
             }
-        })
-	}
+
+            waypoints += `${element.latitude},${element.longitude}`
+        }
+
+        return waypoints
+    }
 
     navigateTo = () => {
         this.props.navigation.navigate('Home');
@@ -69,7 +149,7 @@ class ResultScreen extends Component {
                             </View>
                         </View>
                     </View>
-                    {this.state.journey.length > 0 ? this.state.journey : <Text style={{ padding: 15, borderBottomColor: '#dfdfdf', borderBottomWidth: 1 , width: '100%'}}>No journeys' available</Text>}
+                    {this.state.journey.length > 0 ? this.state.journey : <Text style={{ padding: 15, borderBottomColor: '#dfdfdf', borderBottomWidth: 1, width: '100%' }}>No journeys' available</Text>}
                     <View style={{ alignItems: "center", padding: 25 }}>
                         <Text>Nothing Available?</Text>
                         <Text style={{ color: '#ff0000', marginTop: 5 }}>CONTACT US</Text>
